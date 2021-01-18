7ce9013906af606ab28288f589ecf3e96cad7966
Thanawat.k <thanawat.k@global.ntt>
2021-01-18 13:15:59 +0700

    feat: add mocks service

---
+++ mocks/handlers.js
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+import users from "./data/users"; // contains mock data for users
+import messages from "./data/messages";// contains mock data for messages
+
+export const handlers = [
+  rest.get("https://backend.dev/users", (req, res, ctx) => {
+    return res(ctx.json(users));
+  }),
+
+  rest.get("/users/:id/messages", (req, res, ctx) => {
+    return res(ctx.json(messages));
+  }),
+
+  rest.post("/users/:id/messages", (req, res, ctx) => {
+    const { message } = JSON.parse(req.body);
+    return res(
+      ctx.json({
+        id: Math.random(),
+        message,
+      })
+    );
+  }),
+];
